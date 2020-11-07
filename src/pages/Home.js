d0c48edf828a8a1658920c99ebed0eb719e59c2c
Nathanael Ribeiro <ribeirotomas1904@gmail.com>
2020-11-07 11:50:00 -0300

    Improve search logic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useHistory } from 'react-router-dom';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { getUsers } from '../store/ducks/users';
 
 const Home = () => {
-  const [query, setQuery] = useState('');
+  const initialQuery = window.location.search.split('=')[1];
 
+  const [query, setQuery] = useState(initialQuery || '');
+  const [queryInput, setQueryInput] = useState(initialQuery || '');
+  
   const { users, isLoading, error } = useSelector(state => state.users);
+
+  const history = useHistory();
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (query) {
+      history.push(`/?q=${query}`);
+      dispatch(getUsers({ query }));
+    }
+  }, [query, history, dispatch]);
 
   return (
     <div>
       <form onSubmit={(e) => {
         e.preventDefault();
-        dispatch(getUsers({ query }));
+        setQuery(queryInput);
       }}>
         <input
           type="text"
-          value={query}
-          onChange={e => setQuery(e.target.value)}
+          value={queryInput}
+          onChange={e => setQueryInput(e.target.value)}
         />
         <button>Search</button>
       </form>
